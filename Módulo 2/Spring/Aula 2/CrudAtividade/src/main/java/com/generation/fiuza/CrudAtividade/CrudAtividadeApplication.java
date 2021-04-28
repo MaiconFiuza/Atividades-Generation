ceb55126d46c35cc92debe22cd8f96087b156ca6
Maicon Fiuza <maicon.b.fiuza@gmail.com>
2021-04-28 18:13:48 -0300

    Crud-terminado

---
+++ Módulo 2/Spring/Aula 2/CrudAtividade/src/main/java/com/generation/fiuza/CrudAtividade/CrudAtividadeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.fiuza.CrudAtividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAtividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAtividadeApplication.class, args);
+	}
+
+}
