5e7f2cb57860fcc8f336d10465390e9c301df32a
Maicon Fiuza <maicon.b.fiuza@gmail.com>
2021-05-11 21:44:50 -0300

    Atualizado com JUnit

---
+++ Módulo 2/Spring/Aula 2/CrudAtividade/src/test/java/com/generation/fiuza/CrudAtividade/repository/ContatoRepository.java
@@ -0,0 +1,9 @@
+package com.generation.fiuza.CrudAtividade.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.fiuza.CrudAtividade.model.ContatoModel;
+
+public interface ContatoRepository extends JpaRepository<ContatoModel, Long>{
+
+}
